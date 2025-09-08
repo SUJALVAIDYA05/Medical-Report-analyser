6608aa0e4aa5a0b363cce23ca8bc13031da891c0
SUJALVAIDYA05 <sujalv641@gmail.com>
2025-09-08 20:46:00 +0530

    created frontend and added header and footer to each page

--- frontend/app.js
+++ frontend/app.js
@@ -1,13 +1,27 @@
 import express from "express"
 import path from "path";
+import multer from "multer";
 import { fileURLToPath } from "url"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
 const app =express();
- 
+
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // save files in uploads folder
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname)); // unique file name
+  }
+});
+
+const upload = multer({ storage: storage });
+
 app.use(express.static("public"));
-const port = 3000;
+app.use("/uploads", express.static("uploads"));
+const port = 5000;
 
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname,"views"));
@@ -15,6 +29,15 @@ app.set("views",path.join(__dirname,"views"));
 app.get("/",(req,res)=>{
     res.render("index.ejs");
 })
+
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send("No file uploaded.");
+  }
+  res.send(`Image uploaded successfully: <br><img src="/${req.file.path}" width="200">`);
+});
+
 app.listen(port,()=>{
     console.log(`server running at ${port}`);
 })
